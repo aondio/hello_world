4590d2c43352581db40343adcb80efd952c98477
Aondio <aaondio@88e9fe7064e9.ant.amazon.com>
2018-09-14 10:51:04 +0200

    first commit

---
+++ lambda/eu-west-1_aws-serverless-repository-alexaskillskitnodejsfact-25IPGEZDER3Q/i18n/it.js
@@ -0,0 +1,14 @@
+module.exports = {
+    translation: {
+        SKILL_NAME: 'Hello World',
+        WELCOME_MESSAGE: 'Benvenuto sull'Alexa Skills Kit',
+        HELP_MESSAGE: 'Please guess my number between 0 and 100',
+        GOODBYE: 'Thanks for playing and goodbye!',
+        TOO_HIGH: 'is too high!',
+        TOO_LOW: 'is too low!',
+        SAY_HIGHER: 'Please say a higher number',
+        SAY_LOWER: 'Please say a lower number',
+        CORRECT: 'is right!',
+        ERROR: 'Sorry, I didn\'t catch that. Please say a number'
+    }
+}
